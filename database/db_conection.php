da7ea62eb5d6c30a027a69454c6359e8c14a57e3
Monder <monder_1993@hotmail.com>
2016-05-30 16:46:52 +0300

    database connection file

---
+++ database/db_conection.php
@@ -0,0 +1,8 @@
+<?php
+//-------------------(host,username,password)
+$dbcon=mysqli_connect("localhost","root","","chefsforpeace");
+
+//selecting the database users 
+mysqli_select_db($dbcon,"users");
+
+?>
